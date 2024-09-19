40922d438e86e26601ec06e58eb336208dff1fee
Lady-Bee <bisimi16@gmail.com>
2024-09-19 20:25:27 +0100

    task_2

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import Notifications from "./Notifications";
+import reportWebVitals from "./reportWebVitals";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
